0511570c785198c13aea21737e9c7459833fd58d
Berkay LOSTAR <berkay.lostar@hotmail.com>
2025-06-22 13:46:15 +0300

    Repository fixed

--- PawnLabs.Dpay.Data/RepositoryExtension.cs
+++ PawnLabs.Dpay.Data/RepositoryExtension.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using PawnLabs.Dpay.Data.Repository;
+using PawnLabs.Dpay.Data.Repository.Base;
+using PawnLabs.Dpay.Data.Repository.Base.Impl;
 using PawnLabs.Dpay.Data.Repository.Impl;
 
 namespace PawnLabs.Dpay.Data
@@ -8,6 +10,8 @@ namespace PawnLabs.Dpay.Data
     {
         public static IServiceCollection AddRepositories(this IServiceCollection services)
         {
+            services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped<IPaymentRepository, PaymentRepository>();
             services.AddScoped<IConfigurationRepository, ConfigurationRepository>();
