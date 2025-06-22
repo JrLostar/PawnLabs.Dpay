d5e956f9f640b29ea879bcbec77050d618a4b28d
Berkay LOSTAR <berkay.lostar@hotmail.com>
2025-06-22 10:07:42 +0300

    Queries added

--- PawnLabs.Dpay.Business/BackgroundService/StellarBackgroundService.cs
+++ PawnLabs.Dpay.Business/BackgroundService/StellarBackgroundService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using PawnLabs.Dpay.Business.Service;
 using PawnLabs.Dpay.Core.Configuration;
@@ -17,24 +18,15 @@ namespace PawnLabs.Dpay.Business.BackgroundService
 
         private StellarConfiguration stellarConfiguration;
 
-        private IPaymentService _paymentService;
-        private IProductService _productService;
-        private IConfigurationService _configurationService;
+        private IServiceProvider _serviceProvider;
 
-        private ISocketHelper _socketHelper;
-
-        public StellarBackgroundService(IHttpClientFactory httpClientFactory, IOptions<StellarConfiguration> stellarConfiguration, 
-            IPaymentService paymentService, IProductService productService, IConfigurationService configurationService, ISocketHelper socketHelper)
+        public StellarBackgroundService(IHttpClientFactory httpClientFactory, IOptions<StellarConfiguration> stellarConfiguration, IServiceProvider serviceProvider)
         {
             _httpClientFactory = httpClientFactory;
 
             this.stellarConfiguration = stellarConfiguration.Value;
 
-            _paymentService = paymentService;
-            _productService = productService;
-            _configurationService = configurationService;
-
-            _socketHelper = socketHelper;
+            _serviceProvider = serviceProvider;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -46,93 +38,102 @@ namespace PawnLabs.Dpay.Business.BackgroundService
 
             while (!stoppingToken.IsCancellationRequested)
             {
-                try
+                using (var scope = _serviceProvider.CreateScope())
                 {
-                    var request = new
+                    var paymentService = scope.ServiceProvider.GetRequiredService<IPaymentService>();
+                    var productService = scope.ServiceProvider.GetRequiredService<IProductService>();
+                    var configurationService = scope.ServiceProvider.GetRequiredService<IConfigurationService>();
+
+                    var socketHelper = scope.ServiceProvider.GetRequiredService<ISocketHelper>();
+
+                    try
                     {
-                        jsonrpc = "2.0",
-                        id = 1,
-                        method = "getEvents",
-                        @params = new
+                        var request = new
                         {
-                            startLedger = ledger == 0 ? (await GetLatestLedgerAsync()) - 1000 : ledger,
-                            filters = new[]
+                            jsonrpc = "2.0",
+                            id = 1,
+                            method = "getEvents",
+                            @params = new
                             {
+                                startLedger = ledger == 0 ? (await GetLatestLedgerAsync()) - 1000 : ledger,
+                                filters = new[]
+                                {
                                 new
                                 {
                                     type = "contract",
                                     contractIds = new[] { stellarConfiguration.ContractID }
                                 }
                             },
-                            cursor = cursor,
-                            limit = 10
-                        }
-                    };
+                                cursor = cursor,
+                                limit = 10
+                            }
+                        };
 
-                    var response = await httpClient.PostAsJsonAsync(stellarConfiguration.SorobanBaseUrl, request, cancellationToken: stoppingToken);
-                    var json = await response.Content.ReadAsStringAsync(stoppingToken);
+                        var response = await httpClient.PostAsJsonAsync(stellarConfiguration.SorobanBaseUrl, request, cancellationToken: stoppingToken);
+                        var json = await response.Content.ReadAsStringAsync(stoppingToken);
 
-                    var parsed = JsonDocument.Parse(json);
-                    var events = parsed.RootElement.GetProperty("result").GetProperty("events");
+                        var parsed = JsonDocument.Parse(json);
+                        var events = parsed.RootElement.GetProperty("result").GetProperty("events");
 
-                    ledger = parsed.RootElement.GetProperty("result").GetProperty("latestLedger").GetInt64();
-                    cursor = parsed.RootElement.GetProperty("result").GetProperty("cursor").GetString() ?? string.Empty;
+                        ledger = parsed.RootElement.GetProperty("result").GetProperty("latestLedger").GetInt64();
+                        cursor = parsed.RootElement.GetProperty("result").GetProperty("cursor").GetString() ?? string.Empty;
 
-                    foreach (var ev in events.EnumerateArray())
-                    {
-                        string topicBase64 = ev.GetProperty("topic")[0].GetString() ?? "";
-                        string valueBase64 = ev.GetProperty("value").GetString() ?? "";
-
-                        string topic = DecodeSorobanSymbol(topicBase64);
-                        string value = DecodeSorobanSymbol(valueBase64);
-
-                        var values = value.Split(",");
-
-                        var fromAddress = values[0];
-                        var toAddress = values[1];
-                        var productID = values[2];
-                        var amount = values[3];
-                        var date = values[4];
-
-                        var product = await _productService.GetByID(new Guid(productID));
-
-                        if (product == null)
-                            continue;
-
-                        if (product.Price != float.Parse(amount))
-                            continue;
-
-                        var walletAddress = (string)(await _configurationService.GetByType(new Configuration() { Email = product.Email, Type = EnumConfigurationType.WalletAddress })).Value;
-
-                        if (!walletAddress.Equals(toAddress))
-                            continue;
-
-                        var paymentID = await _paymentService.Add(new Payment()
+                        foreach (var ev in events.EnumerateArray())
                         {
-                            Email = product.Email,
-                            ProductID = product.ID,
-                            BuyerAddress = fromAddress,
-                            WalletAddress = walletAddress,
-                            Price = product.Price,
-                            PriceType = product.PriceType,
-                            Date = DateTimeOffset.FromUnixTimeSeconds(long.Parse(date)).UtcDateTime
-                        });
+                            string topicBase64 = ev.GetProperty("topic")[0].GetString() ?? "";
+                            string valueBase64 = ev.GetProperty("value").GetString() ?? "";
 
-                        if (!paymentID.HasValue)
-                            continue;
+                            string topic = DecodeSorobanSymbol(topicBase64);
+                            string value = DecodeSorobanSymbol(valueBase64);
 
-                        #region Socket
+                            var values = value.Split(",");
 
-                        await _socketHelper.ConnectToHub(product.Email);
-                        await _socketHelper.Connection.InvokeAsync("Payment", product.ID);
-                        await _socketHelper.DisconnectFromHub();
+                            var fromAddress = values[0];
+                            var toAddress = values[1];
+                            var productID = values[2];
+                            var amount = values[3];
+                            var date = values[4];
 
-                        #endregion
+                            var product = await productService.GetByID(new Guid(productID));
+
+                            if (product == null)
+                                continue;
+
+                            if (product.Price != float.Parse(amount))
+                                continue;
+
+                            var walletAddress = (string)(await configurationService.GetByType(new Configuration() { Email = product.Email, Type = EnumConfigurationType.WalletAddress })).Value;
+
+                            if (!walletAddress.Equals(toAddress))
+                                continue;
+
+                            var paymentID = await paymentService.Add(new Payment()
+                            {
+                                Email = product.Email,
+                                ProductID = product.ID,
+                                BuyerAddress = fromAddress,
+                                WalletAddress = walletAddress,
+                                Price = product.Price,
+                                PriceType = product.PriceType,
+                                Date = DateTimeOffset.FromUnixTimeSeconds(long.Parse(date)).UtcDateTime
+                            });
+
+                            if (!paymentID.HasValue)
+                                continue;
+
+                            #region Socket
+
+                            await socketHelper.ConnectToHub(product.Email);
+                            await socketHelper.Connection.InvokeAsync("Payment", product.ID);
+                            await socketHelper.DisconnectFromHub();
+
+                            #endregion
+                        }
                     }
-                }
-                catch (Exception ex)
-                {
-                    break;
+                    catch (Exception ex)
+                    {
+                        break;
+                    }
                 }
 
                 await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
